1f0eca36243447d77373c209b655338688ae503b
Kaptan <kaptanbamaniya@gmail.com>
2026-02-10 00:41:08 -0800

    Initial Commit

---
+++ Stomach friend/Stomach_friendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stomach_friendApp.swift
+//  Stomach friend
+//
+//  Created by applelab03 on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct Stomach_friendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
