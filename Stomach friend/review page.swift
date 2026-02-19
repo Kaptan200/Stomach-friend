d4a61f18e9f96415189a62fd25f4ab043160881b
Kaptan <kaptanbamaniya@gmail.com>
2026-02-18 22:21:47 -0800

    made a review page

---
+++ Stomach friend/review page.swift
@@ -0,0 +1,77 @@
+//
+//  review page.swift
+//  Stomach friend
+//
+//  Created by applelab03 on 2/18/26.
+//
+import SwiftUI
+struct review: Identifiable{
+    let id = UUID()
+    let name: String
+    let rating: Int
+    let comment: String
+}
+struct review_page: View {
+    @State private var name: String = ""
+    @State private var rating: Int = 0
+    @State private var comment: String = ""
+    @State private var reviews: [review] = []
+    var body: some View {
+        VStack{
+            VStack(alignment: .leading,spacing: 12){
+                Text("write a review")
+                    .font(.title2)
+                    .bold()
+                TextField("NAME", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                HStack{
+                    ForEach(1...5, id: \.self) { star in
+                        Image(systemName: star <= rating ? "star.fill" : "star")
+                            .foregroundColor(.yellow)
+                            .onTapGesture {
+                                rating = star
+                            }
+                        
+                    }
+                }
+               TextField("WRITE YOUR REVIEW HERE", text: $comment)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                Button("submit"){
+                    let newReview = review(name: name, rating: rating, comment: comment)
+                    reviews.append(newReview)
+                }.frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color(.systemBlue))
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            
+                }
+            List(reviews){review in
+                VStack(alignment: .leading, spacing: 8){
+                    HStack{
+                        Text(review.name)
+                            .font(.headline)
+                        HStack{
+                            ForEach(1...5, id: \.self) { star in
+                                if star <= review.rating {
+                                    Image(systemName: "star.fill")
+                                        .foregroundStyle(Color(.yellow))
+                                } else {
+                                    Image(systemName: "star")
+                                }
+                            }
+                        }
+                    }
+                    Text(review.comment)
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+                .padding(.vertical, 5)
+            }
+        }.navigationTitle("restaurant reviews")
+        }
+    }
+
+#Preview {
+    review_page()
+}
